ecfd2be40d911903bdca6ed757d37833fff7e441
Ahmad-Nouh <ahmadnouh428@gmail.com>
2019-05-14 18:11:41 +0200

    first push

--- parallel-programming-project/parallel-programming-project/Source.cpp
+++ parallel-programming-project/parallel-programming-project/Source.cpp
@@ -2,28 +2,206 @@
 #include <stdio.h>
 #include <iostream>
 #include <math.h>
+#include <vector>
+#include <ctime>
+
+using namespace std;
+
+int generateRandNumber(int, int);
+int generateScore();
+int play();
 
 int main(int argc, char** argv) {
 	// Initialize the MPI environment
-	MPI_Init(NULL, NULL);
-
+	MPI_Init(&argc, &argv);
+	MPI_Status stat;
+	vector<int> toPlay, temp;
 	// Get the number of processes
-	int world_size;
-	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	int numberOfPlayers, rc, myRank, incomingScore = -1, maxScore = -1, maxScoreIndex, maxScoreRank, asyncCount = 0;
+	bool hasLost = false, put_back = false;
+	MPI_Comm_size(MPI_COMM_WORLD, &numberOfPlayers);
 	
 	// Get the rank of the process
-	int world_rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+	srand(time(nullptr) + myRank);
+	int myScore = generateScore();
+    // Initializing toPlay array for first round
+	for (int i = 0; i < numberOfPlayers; i++)
+	{
+		toPlay.push_back(i);
+	}
 
-	// Get the name of the processor
-	char processor_name[MPI_MAX_PROCESSOR_NAME];
-	int name_len;
-	MPI_Get_processor_name(processor_name, &name_len);
 
-	// Print off a hello world message
-	printf("Hello world from processor %s, rank %d out of %d processors\n",
-		processor_name, world_rank, world_size);
+	while (toPlay.size() > 1 && !hasLost)
+	{
+		put_back = false;
+
+		if (toPlay.size() % 2 != 0)
+		{
+			put_back = true;
+
+			for (auto it = toPlay.begin(); it != toPlay.end(); ++it)
+			{
+				int i = distance(toPlay.begin(), it);
+
+				if (toPlay[i] != myRank)
+				{
+					MPI_Send(&myScore, 1, MPI_INT, toPlay[i], 0, MPI_COMM_WORLD);
+				}
+			}
+
+			for (auto it = toPlay.begin(); it != toPlay.end(); ++it)
+			{
+				int i = distance(toPlay.begin(), it);
+
+				if (toPlay[i] != myRank)
+				{
+					MPI_Recv(&incomingScore, 1, MPI_INT, toPlay[i], 0, MPI_COMM_WORLD, &stat);
+				}
+
+				if (incomingScore > maxScore) {
+					maxScore = incomingScore;
+					maxScoreIndex = i;
+					maxScoreRank = toPlay[i];
+				}
+			}
+
+			toPlay.erase(toPlay.begin() + maxScoreIndex);
+		}
+
+		vector<int>::iterator it = find(toPlay.begin(), toPlay.end(), myRank);
+		int index = distance(toPlay.begin(), it);
+		bool opponentLost = false;
+
+		if (index % 2 == 0)
+		{
+			while (!hasLost && !opponentLost)
+			{
+				int myGuessedNumber = play(), opponentGuessedNumber;
+				MPI_Send(&myGuessedNumber, 1, MPI_INT, toPlay[index + 1], 1, MPI_COMM_WORLD);
+				MPI_Recv(&opponentGuessedNumber, 1, MPI_INT, toPlay[index + 1], 1, MPI_COMM_WORLD, &stat);
+
+				if (myGuessedNumber <= opponentGuessedNumber)
+				{
+					myScore--;
+					hasLost = myScore == 0;
+				}
+				else 
+				{
+					myScore++;
+				}
+
+				//if (myRank == 0) {
+				//	cout << "My score: " << myScore << endl;
+				//}
+
+				if (hasLost) {
+					cout << "Player No. " << myRank << " has lost to player No. " << toPlay[index + 1] << endl;
+				}
+
+				MPI_Send(&hasLost, 1, MPI_C_BOOL, toPlay[index + 1], 2, MPI_COMM_WORLD);
+				MPI_Recv(&opponentLost, 1, MPI_C_BOOL, toPlay[index + 1], 2, MPI_COMM_WORLD, &stat);
+			}
+		}
+		else
+		{
+			while (!hasLost && !opponentLost)
+			{
+				int myGuessedNumber = play(), opponentGuessedNumber;
+				MPI_Recv(&opponentGuessedNumber, 1, MPI_INT, toPlay[index - 1], 1, MPI_COMM_WORLD, &stat);
+				MPI_Send(&myGuessedNumber, 1, MPI_INT, toPlay[index - 1], 1, MPI_COMM_WORLD);
+
+				if (myGuessedNumber <= opponentGuessedNumber)
+				{
+					myScore--;
+					hasLost = myScore == 0;
+				}
+				else
+				{
+					myScore++;
+				}
+
+				if (hasLost) {
+					cout << "Player No. " << myRank << " has lost to player No. " << toPlay[index - 1] << endl;
+				}
+
+				MPI_Recv(&opponentLost, 1, MPI_C_BOOL, toPlay[index - 1], 2, MPI_COMM_WORLD, &stat);
+				MPI_Send(&hasLost, 1, MPI_C_BOOL, toPlay[index - 1], 2, MPI_COMM_WORLD);
+			}
+		}
+
+		for (vector<int>::iterator it = toPlay.begin(); it != toPlay.end(); ++it)
+		{
+			int i = distance(toPlay.begin(), it);
+
+			if (*it != myRank)
+			{
+				MPI_Send(&hasLost, 1, MPI_C_BOOL, *it, 3, MPI_COMM_WORLD);
+			}
+		}
+
+		
+		for (vector<int>:: iterator it = toPlay.begin(); it != toPlay.end(); ++it) {
+			if (*it != myRank)
+			{
+				MPI_Recv(&opponentLost, 1, MPI_C_BOOL, *it, 3, MPI_COMM_WORLD, &stat);
+
+				if (opponentLost)
+				{
+					cout << toPlay.size() << " Before\n";
+					
+					it = toPlay.erase(it);
+
+					if (it == toPlay.end()) {
+						break;
+					}
+					
+					cout << toPlay.size() << " After\n";
+				}
+
+			}
+		}
+
+		/*
+		it = toPlay.begin();
+		while (it != toPlay.end())
+		{
+			int i = distance(toPlay.begin(), it);
+
+			if (toPlay[i] != myRank)
+			{
+				MPI_Recv(&opponentLost, 1, MPI_C_BOOL, toPlay[i], 3, MPI_COMM_WORLD, &stat);
+
+				if (!opponentLost)
+				{
+					temp.push_back(toPlay[i]);
+				}
+				
+			}
+			
+			it++;
+		}
+		toPlay = temp;
+		*/
+		//if (put_back) {
+		//	toPlay.insert(toPlay.begin() + maxScoreIndex + 1, maxScoreRank);
+		//}
+	}
 
 	// Finalize the MPI environment.
 	MPI_Finalize();
+}
+
+
+int generateRandNumber(int min, int max) {
+	int randNum = rand() % (max - min + 1) + min;
+	return randNum;
+}
+
+int generateScore() {
+	return generateRandNumber(10, 20);
+}
+
+int play() {
+	return generateRandNumber(5, 10);
 }
